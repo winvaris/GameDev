5d1469bfd8a6e9421cf213c339daa09569b5bf25
Varis <win3455@hotmail.com>
2017-03-27 23:38:18 +0700

    Random

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,10 @@ public class PlayerController : MonoBehaviour {
 	Rigidbody2D rb2d;
 
 	public float speed = 50f;
-	public float maxSpeed = 2.2f;
+	public float maxSpeed = 2;
 	public float jumpPower = 500f;
 	public bool grounded;
+	private float temp = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -22,15 +23,33 @@ public class PlayerController : MonoBehaviour {
 			rb2d.AddForce (Vector2.up * jumpPower);
 		}
 	}
+		
+	bool IsNotDecreasing () {
+		if (temp > 0f) {
+			if (Input.GetAxis ("Horizontal") < temp) {
+				temp = Input.GetAxis ("Horizontal");
+				return false;
+			}
+		}
+		else if (temp < 0f) {
+			if (Input.GetAxis ("Horizontal") > temp) {
+				temp = Input.GetAxis ("Horizontal");
+				return false;
+			}
+		}
+		temp = Input.GetAxis ("Horizontal");
+		return true;
+	}
 
 	void FixedUpdate () {
 		float h = Input.GetAxis ("Horizontal");
-
 		// Move the player
-		rb2d.AddForce (Vector2.right * speed * h);
-
-		Debug.Log (h);
-
+		if (IsNotDecreasing ()) {
+			rb2d.AddForce (Vector2.right * speed * h);
+		}
+		if (Input.GetKeyUp (KeyCode.A) || Input.GetKeyUp (KeyCode.D)) {
+			rb2d.velocity = new Vector3 (0f, 0f, 0f);
+		}
 		// Limiting the speed of the player
 		if (rb2d.velocity.x > maxSpeed) {
 			rb2d.velocity = new Vector2 (maxSpeed, rb2d.velocity.y);
